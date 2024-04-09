dc72c66b4521ee71d8dc7aa1c370f88de732dcfa
Bernhard <128181584+BigBurneXX@users.noreply.github.com>
2024-04-09 13:23:32 +0200

    creating basic project structure

---
+++ backend/src/main/java/com/example/VehicleRoutingProblem/VehicleRoutingProblemApplication.java
@@ -0,0 +1,13 @@
+package com.example.VehicleRoutingProblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRoutingProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRoutingProblemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/VehicleRoutingProblem/VehicleRoutingProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VehicleRoutingProblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRoutingProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
